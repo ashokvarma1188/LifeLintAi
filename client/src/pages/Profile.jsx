9698d81ed7fd15b626c01b6c312c4eef7b4f6afa
ashokvarma1188 <ashokvarma527@gmail.com>
2026-08-13 12:12:51 +0530

    Fix case-sensitive import breaking Linux build

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import AppNavbar from "./AppNavbar";
 import "./Dashboard.css";
-import "./Auth.css";
 import "./Profile.css";
 
 function Profile() {
